efef5a7b6168a09acaf582fe23fa039fc2e97b2c
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-18 12:59:28 -0700

    add services component

--- apps/portfolio/src/app/common/services/experiences.service.ts
+++ apps/portfolio/src/app/common/services/experiences.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Experience } from '../models/experience';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExperiencesService {
-
-  constructor() { }
+  experiences: Experience[] = [
+    {
+      id: '1',
+      title: 'Store Manager Trainer',
+      description: 'Trained Starbucks Store Managers to be awesome! ',
+      current: false,
+    },
+    {
+      id: '2',
+      title: 'District Sales Manager',
+      description: 'Managed multiple retail locations across the US ',
+      current: false,
+    },
+  ];
 }
